f2f7383f9a09058d2dee9ebefd7899a0f87faa42
Ubuntu <ubuntu@ip-10-0-2-218.ap-southeast-1.compute.internal>
2020-02-25 06:51:28 +0000

    Initial import

---
+++ js/research.js
@@ -0,0 +1,47 @@
+// Changes height of target to newHeight
+
+function changeHeightByClass(target, newHeight) {
+    var elements = document.getElementsByClassName(target)
+    var i
+    for (i = 0; i < elements.length; i++) {
+        elements[i].setAttribute("data-height", newHeight)
+        elements[i].style.height = newHeight
+    }
+}
+
+// Media queries
+
+if (matchMedia) {
+    var mediaQuery600 = window.matchMedia("(max-width: 600px)")
+    lessThan600px(mediaQuery600)
+    mediaQuery600.addListener(lessThan600px)
+
+    var mediaQuery900 = window.matchMedia("(max-width: 900px)")
+    lessThan900px(mediaQuery900)
+    mediaQuery900.addListener(lessThan900px)
+}
+
+//General mobile changes
+function lessThan600px(mediaQuery) {
+    if (mediaQuery.matches) {
+        // Header
+        removeClassById("header-text", "u-size7of8")
+    } else {
+        // Header
+        addClassById("header-text", "u-size7of8")
+    }
+}
+
+// GBS Figure and legend - emoji cards
+function lessThan900px(mediaQuery) {
+    if (mediaQuery.matches) {
+        removeClassByClass("emoji-card", "u-size1of2")
+        addClassByClass("emoji-card", "sizeFull")
+        addClassByClass("emoji-card", "center-margin")
+    } else {
+        addClassByClass("emoji-card", "u-size1of2")
+        removeClassByClass("emoji-card", "sizeFull")
+        removeClassByClass("emoji-card", "center-margin")
+    }
+}
+
